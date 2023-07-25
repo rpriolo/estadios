2607a42482b0fe0042a6e899d16d7ef5c6848388
Renato Priolo <rpriolo@gmail.com>
2023-07-25 15:10:31 -0300

    Implementação dos métodos de busca de estádios por nome e time

--- src/main/java/br/com/rpriolo/estadios/view/Main.java
+++ src/main/java/br/com/rpriolo/estadios/view/Main.java
@@ -1,15 +1,26 @@
 package br.com.rpriolo.estadios.view;
 
 import br.com.rpriolo.estadios.controller.BuscaApi;
+import br.com.rpriolo.estadios.model.Estadio;
+import com.google.gson.Gson;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         BuscaApi busca = new BuscaApi();
-        System.out.println(busca.buscarTodosOsEstadios());
-        System.out.println("-----");
-        String resultado = busca.buscarEstadiosPorNome("Vila Belmiro");
+//        System.out.println(busca.buscarTodosOsEstadios());
+//        System.out.println("-----");
+        String resultado = busca.buscarEstadiosPorNome("parque");
         System.out.println(resultado);
-        System.out.println("-----");
-        System.out.println(busca.buscarEstadiosPorTime("São Paulo"));
+//        System.out.println("-----");
+//        System.out.println(busca.buscarEstadiosPorTime("São Paulo"));
+
+        Gson gson = new Gson();
+        Estadio[] listaEstadios = gson.fromJson(resultado, Estadio[].class);
+
+        for (Estadio estadio : listaEstadios) {
+            System.out.println(estadio);
+        }
     }
 }
