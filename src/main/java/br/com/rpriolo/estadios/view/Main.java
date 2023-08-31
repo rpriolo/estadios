91e80fd5693d6b1c927a9d134179742ef5b2a642
Renato Priolo <rpriolo@gmail.com>
2023-08-31 18:19:47 -0300

    Refatoração do código do menu principal

--- src/main/java/br/com/rpriolo/estadios/view/Main.java
+++ src/main/java/br/com/rpriolo/estadios/view/Main.java
@@ -1,26 +1,60 @@
 package br.com.rpriolo.estadios.view;
 
 import br.com.rpriolo.estadios.controller.BuscaApi;
-import br.com.rpriolo.estadios.model.Estadio;
-import com.google.gson.Gson;
+import br.com.rpriolo.estadios.controller.FormatadorJson;
 
-import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+    public static BuscaApi busca = new BuscaApi();
+
     public static void main(String[] args) {
-        BuscaApi busca = new BuscaApi();
-//        System.out.println(busca.buscarTodosOsEstadios());
-//        System.out.println("-----");
-        String resultado = busca.buscarEstadiosPorNome("parque");
-        System.out.println(resultado);
-//        System.out.println("-----");
-//        System.out.println(busca.buscarEstadiosPorTime("São Paulo"));
+        exibirMenu();
+        scanner.close();
+    }
 
-        Gson gson = new Gson();
-        Estadio[] listaEstadios = gson.fromJson(resultado, Estadio[].class);
+    public static void exibirMenu() {
+        System.out.println("""
+                \n--- MENU ---
+                Selecione uma opção:
+                1 - Buscar todos os estádios
+                2 - Buscar estádios por nome
+                3 - Buscar estádios por time
+                4 - Sair""");
+        System.out.print("OPÇÃO: ");
+        escolherOpcao();
+    }
 
-        for (Estadio estadio : listaEstadios) {
-            System.out.println(estadio);
+    public static void escolherOpcao() {
+        int opcao = scanner.nextInt();
+
+        String json;
+        switch (opcao) {
+            case 1:
+                json = busca.buscarTodosOsEstadios();
+                FormatadorJson.formatarJson(json);
+                break;
+            case 2:
+                System.out.print("Digite o nome do estádio que deseja buscar: ");
+                scanner.nextLine();
+                String estadioPesquisado = scanner.nextLine();
+                json = busca.buscarEstadiosPorNome(estadioPesquisado);
+                FormatadorJson.formatarJson(json);
+                break;
+            case 3:
+                System.out.print("Digite o nome do time que deseja buscar: ");
+                scanner.nextLine();
+                String timePesquisado = scanner.nextLine();
+                json = busca.buscarEstadiosPorTime(timePesquisado);
+                FormatadorJson.formatarJson(json);
+                break;
+            case 4:
+                return;
+            default:
+                System.out.println("A opção digitada não é válida. Tente novamente.");
+                break;
         }
+        exibirMenu();
     }
 }
